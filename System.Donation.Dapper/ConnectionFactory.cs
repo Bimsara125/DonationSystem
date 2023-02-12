8492edd0ab3fddd42e3f7c3c1ea365323563ffab
Bimsara <bimsara125@gmail.com>
2023-02-12 21:12:45 +0530

    Commit 1

---
+++ System.Donation.Dapper/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Donation.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Donation.Dapper
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        private readonly string connectionString;
+        public ConnectionFactory(string connectionString) 
+        {
+            this.connectionString = connectionString;       
+        }
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(this.connectionString);
+        }
+    }
+}
---
+++ System.Donation.Services/DonorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Donation.Core.IServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Donation.Services
+{
+    public class DonorService : IDonorService
+    {
+
+    }
+}
